b6e8958176db8ea524ef750b32977edc26210a4e
HuyCam <camhuy165@gmail.com>
2021-07-16 13:21:00 -0500

    add a explanation

--- src/main/java/com/example/springbatchdemo/batch/DBWriter.java
+++ src/main/java/com/example/springbatchdemo/batch/DBWriter.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+/*
+Item write use repository to write user to db (h2)
+ */
 @Component
 public class DBWriter implements ItemWriter<User> {
     @Autowired
